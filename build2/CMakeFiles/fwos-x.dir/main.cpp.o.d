aa2f4d759b11fb9846cf7642107a876d84629e4b
Tejas Shinde <tejshinde14@gmail.com>
2026-05-24 17:55:14 +0000

    remove generated build files from repository

--- build2/CMakeFiles/fwos-x.dir/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/fwos-x.dir/main.cpp.o: \
- /mnt/data/fwproj/Distributed-Firewall-Control-Plane-OS-main/main.cpp \
- /usr/include/stdc-predef.h \
- /mnt/data/fwproj/Distributed-Firewall-Control-Plane-OS-main/core/control_plane.hpp
